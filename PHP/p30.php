35e0837c9f4dfa91c31299fdf17d799572b30654
Shubha-Maurya <sm466287@gmail.com>
2025-09-15 22:22:43 +0530

    Add files via upload

---
+++ PHP/p30.php
@@ -0,0 +1,11 @@
+<?php
+$countries = array(
+    "India" => "New Delhi",
+    "usa" => "Washington D.C.",
+    "Japan" => "Tokyo",
+    "Brazil" => "Brasilia"
+);
+foreach ($countries as $country => $capital) {
+    echo "The capital of $country is $capital.<br>";
+}
+?>
