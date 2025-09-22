9758ba1da6a43d77f19f5294fc67ad8006c7227d
Shubha-Maurya <sm466287@gmail.com>
2025-09-22 20:33:04 +0530

    Add files via upload

---
+++ PHP/p37.php
@@ -0,0 +1,13 @@
+<?php
+if (isset($_GET['name']) &&
+isset($_GET['age'])) {
+    $name =
+htmlspecialchars($_GET['name']);
+     $age=
+htmlspecialchars($_GET['age']);
+     echo "Hello $name, you are $age years old.";      
+}
+else{
+    echo "Please provide both name and age in the URL.";
+}
+?>
